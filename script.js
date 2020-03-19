25fb3caeff97412c11b4f42171bc2d4531d84dd1
Marjolaine Baratte <marjolaine.baratte@gmail.com>
2020-03-19 17:27:03 +0100

    enlarging and reduce photos

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+// Animation bouton Send
 const select = document.querySelector('.homeButton')
 
 select.addEventListener('click',function(){
     alert ('Votre message a bien été envoyé par pigeaon voyageur 🐦!')})
+
+// Animation boutons Agrandir et Réduire
+const reduc = document.getElementById('reduire')
+const bloc = document.querySelector('.blocLast')
+reduc.addEventListener('click',function(){
+    bloc.classList.add("hide")})
+
+const agrand = document.getElementById('agrandir')
+agrand.addEventListener('click',function(){
+    bloc.classList.remove("hide")})
+    
